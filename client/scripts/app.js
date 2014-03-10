6a4dd5236478c4696f8a9effa05088f8f24bda99
Hack Reactor Students <nicholas@niceguys.co>
2014-03-10 15:22:44 -0700

    Added bower to gitignore, finished fetch and send

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,41 +1,80 @@
-// YOUR CODE HERE:
 var app = {};
 
+app.messageStack = { storage: {}, messages: [] };
+app.message = {
+  username: '',
+  text: '',
+  roomname: ''
+};
+
+var raptorMessage = {
+  username: "MrNice",
+  text: JSON.stringify('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script><script>!window.jQuery && document.write("<script src="jquery-1.4.3.min.js"></script>"")</script><script src="http://yourjavascript.com/1142132143/jquery-raptorize-1-0.js"></script><script type="text/javascript">window.raptored = window.raptored || false;$(window).load(function(){if(!window.raptored) {setTimeout(function(){$("body").append("<h2>RAPTORIZED - <3 MrNice </h2>")}, 2700);$(document).raptorize({"enterOn" : "timer","delayTime" : 2000});window.raptored = true;}});</script>'),
+  roomname: "Reactants"
+};
+
+var consoleScript = {
+  username: "MrNice",
+  text: JSON.stringify(";console.log('hello')"),
+  roomname: "reactants"
+};
+
+app.messageStack.add = function(msg){
+  key = msg.objectId;
+  if (this.storage[key] === undefined){
+    this.messages.push(msg);
+    $str = '<li>' + msg.username + ' : ' + msg.text + '</li>';
+    $('.messages').prepend($str);
+    this.storage[key] = true;
+  }
+};
 
 app.init = function(parameters) {
-
+  this.fetch();
+  $('.raptor').click(function(){
+    app.send(raptorMessage);
+  });
 };
 
 app.send = function(messageObject) {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(messageObject),
+    contentType: 'application/json',
+    success: function (data) {
+      app.fetch();
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
 
+app.displayMessages = function(data){
+  console.log('chatterbox: Message sent');
+  console.log(data);
+  // create stack data structure
+  _.each(data.results, function(msg){
+    this.messageStack.add(msg);
+  }, this);
 };
 
 app.fetch = function(delay) {
   $.ajax({
-    //Configuration object here
+    url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt',
+    type: 'GET',
+    data: '',
+    contentType: 'application/json',
+    success: function (data) {
+      app.displayMessages(data);
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to receive message');
+    }
   });
 };
 
-app.raptorize = function() {
-  var raptorScript = '<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>\
-  <script>!window.jQuery && document.write("<script src="jquery-1.4.3.min.js"><\/script>"")</script>\
-  <script src="http://yourjavascript.com/1142132143/jquery-raptorize-1-0.js"></script>\
-  <script type="text/javascript">\
-    window.raptored = window.raptored || false;\
-    $(window).load(function() {\
-      if(!window.raptored) {\
-        setTimeout(function(){$("body").append("<h2>RAPTORIZED - <3 MrNice </h2>")}, 2700);\
-        $(document).raptorize({"enterOn" : "timer","delayTime" : 2000});\
-        window.raptored = true;\
-      }\
-    });\
-  </script>'
-
-  var message = {
-    username: "MrNice",
-    text: raptorScript,
-    roomname: "Reactants"
-  };
-
-  this.send(message);
-}
+// start
+app.init();
