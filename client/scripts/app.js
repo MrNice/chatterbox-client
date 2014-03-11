69d81fb577a9031159149e1d5023c9d0a277fde8
Hack Reactor Students <nicholas@niceguys.co>
2014-03-10 17:36:11 -0700

    Refactored to have chat rooms

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,35 +1,56 @@
 var app = {};
-app.server = 'https://api.parse.com/1/classes/chatterbox?order=-createdAt';
 
+app.server = 'https://api.parse.com/1/classes/chatterbox?order=-createdAt';
 app.messageStack = { storage: {}, messages: [] };
 app.message = {
   username: '',
   text: '',
   roomname: ''
 };
-
+app.chatrooms = {};
 
 app.messageStack.add = function(msg){
   key = msg.objectId;
   if (this.storage[key] === undefined){
+    app.chatrooms.add(msg);
     this.messages.push(msg);
-    $str = $('<li></li>');
-    $str.text(msg.username + ": " + msg.text);
-    // $str = '<li>' + msg.username + ": " + msg.text + '</li>';
-    $('.messages').prepend($str);
+
     this.storage[key] = true;
   }
 };
 
+app.chatrooms.add = function(msg){
+  if(msg.roomname === undefined){
+    throw 'OMFGERROR';
+  } else {
+    if(this[msg.roomname] !== undefined){
+      this[msg.roomname].push(msg);
+    } else {
+      this[msg.roomname] = [msg];
+      var $str = $('<li></li>');
+      var saltedName = '123' + msg.roomname;
+      $str.text(msg.roomname);
+      $str.attr("id", saltedName);
+      $('#roomlist').append($str);
+      $('#' + saltedName).click(function(event) {
+        console.log('Chatroom changed');
+        $('.messages').empty();
+        app.displayMessages(app.chatrooms[saltedName.substr(3)]);
+      });
+    }
+  }
+};
+
 app.init = function(parameters) {
+  app.username = QueryString.username;
   this.fetch();
-  $('.raptor').click(function(){
-    app.send(raptorMessage);
-  });
   setInterval(app.fetch, 3000);
 };
 
 app.send = function(messageObject) {
+  if(messageObject.text === '') {
+    throw "No Message Error";
+  }
   $.ajax({
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'POST',
@@ -51,6 +72,10 @@ app.fetch = function(delay) {
     data: '',
     contentType: 'application/json',
     success: function (data) {
+      data = data.results;
+      _(data.results).each(function(msg){
+       this.messageStack.add(msg); 
+      }, this);
       app.displayMessages(data);
     },
     error: function (data) {
@@ -60,31 +85,43 @@ app.fetch = function(delay) {
   });
 };
 
-app.displayMessages = function(data){
-  console.log('chatterbox: Message sent');
-  console.log(data);
-  // create stack data structure
-  _.each(data.results, function(msg){
-    this.messageStack.add(msg);
+app.displayMessages = function(messageList){
+  //Takes an array of messages, and puts them onto the screen
+  _(messageList).each(function(msg){
+    var $str = $('<li></li>');
+    $str.text(msg.username + ": " + msg.text);
+    $('.messages').prepend($str);
   }, this);
 };
 
-var raptorMessage = {
-  username: "MrNice",
-  text: '<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script><script>!window.jQuery && document.write("<script src="jquery-1.4.3.min.js"></script>"")</script><script src="http://yourjavascript.com/1142132143/jquery-raptorize-1-0.js"></script><script type="text/javascript">window.raptored = window.raptored || false;$(window).load(function(){if(!window.raptored) {setTimeout(function(){$("body").append("<h2>RAPTORIZED - <3 MrNice </h2>")}, 2700);$(document).raptorize({"enterOn" : "timer","delayTime" : 2000});window.raptored = true;}});</script>',
-  roomname: "Reactants"
+// returns the message object
+app.createMessage = function(){
+  var msg = Object.create(app.message);
+  msg.username = app.username;
+  msg.text = $('.message').val();
+  msg.roomname = 'Default';
+  $('.message').val('');
+  return msg;
 };
 
-var consoleScript = {
-  username: "MrNice",
-  text: JSON.stringify("<script>console.log('hello')</script>"),
-  roomname: "reactants"
-};
 // start
 $(document).ready(function() {
-  $('.raptor').click(function(){
-    app.send(raptorMessage);
+  $('.username').keyup(function(event){
+    if (event.which === 13){
+      app.username = $('.username').val();
+      $('.username').val('');
+    }
   });
-});
 
-app.init();
+  $('.message').keyup(function(event){
+    if (event.which === 13){
+      app.send( app.createMessage() );
+    }
+  });
+
+  $('.send').click(function(){
+    app.send( app.createMessage() );
+  });
+
+  app.init();
+});
